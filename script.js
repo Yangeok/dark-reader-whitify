72094af8a1a1d92151bfeacbb1eee62cfd2f953a
Yangwook <wooky92@naver.com>
2019-04-25 13:12:12 +0900

    -

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
 window.onload = () => {
   chrome.tabs.executeScript(
     {
-      code:
-        'const body = document.querySelector("body");const el = document.getElementsByClassName("st");console.log(el);if(window.location.protocol == "http:" || "http:") {body.style.color = "black";for(let i = 0; i < el.length; i++){el[i].style.color = "black";}}'
+      code: `
+        const body = document.querySelector('body');
+        const ps = document.querySelectorAll('p');
+        const googles = document.getElementsByClassName('st');
+        const feedlies = document.getElementsByClassName('summary');
+        const oneBoons = document.getElementsByClassName('detail_editor');
+
+        if(window.location.protocol == 'http:' || 'http:') {
+          body.style.color = 'black';
+        }
+        
+        for(p of ps) {
+          p.style.color = 'black'
+        }
+
+        for(google of googles) {
+          google.style.color = 'black';
+        }
+
+        for(feedly of feedlies) {
+          feedly.style.color = 'black';
+        }
+
+        for(oneBoon of oneBoons) {
+          oneBoon.style.color = 'black';
+        }
+        `
     },
     () => {
-      document.querySelector('body').innerHTML =
-        '<img src="./icons/title.png"/><p/><div>Successful font color change to "BLACK"</div>';
+      document.querySelector('body').innerHTML = `
+          <img src='./icons/title.png'/>
+          <p/>
+          <div>
+            Successful font color change to 'BLACK'
+          </div>
+        `;
     }
   );
 };
